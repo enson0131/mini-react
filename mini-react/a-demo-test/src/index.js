1637884d12ea09ad79a02f3e7227454fc54017f0
enson_chen <1004714132@qq.com>
2023-04-06 19:43:54 +0800

    feat: ✨ 更改 react 调试代码

--- mini-react/a-demo-test/src/index.js
+++ mini-react/a-demo-test/src/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// https://reactjs.org/blog/2020/10/20/react-v17.html
+ReactDOM.render(<App />, document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

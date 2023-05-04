ccba31abc2ab1ed27ab0454131f87ffa57b9bd5d
enson_chen <1004714132@qq.com>
2023-05-04 11:59:59 +0800

    feat: ✨ React 更新机制

--- mini-react/a-demo-test/src/异步更新机制.js
+++ mini-react/a-demo-test/src/异步更新机制.js
@@ -8,6 +8,14 @@ const reducer = (state, action) => {
     return action
 
 }
+
+/**
+ * React 的异步更新机制其实就是通过一个全局开关（executionContext）判断是否需要开启批处理 
+ * React 会维护一个异步更新队列 SyncQueue, 将需要更新的任务存放到队列中
+ * 从对应的 Fiber 节点出发，不断地向上收集对应的事件，直到 Root 节点为止
+ * 然后在通过 Scheduler_scheduleCallback （微任务）调度任务
+ * 
+ */
 function App() {
   const [count, setCount] = useState(0);
   // const [count, setCount] = useReducer(reducer, 0);
